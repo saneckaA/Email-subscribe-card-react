f27d835adeb7e518bc60f000e5b1849feec57454
Aleksandra Sanecka <a.saneckaa@gmail.com>
2023-11-30 14:56:25 +0100

    made Card component and imported Content in it

---
+++ src/Card/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Content from '../Content';
+
+const Card = () => {
+  return (
+    <div className="bg-zinc-800 p-2 mx-6 rounded-2xl">
+        <Content />
+    </div>
+  )
+}
+
+export default Card;
